61382c6af1761b3cea8f2d27889366ad19a3d9d0
ciminoce <ciminoce@yahoo.com.ar>
2022-06-07 10:17:51 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleAppEjercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número entero:");
+            
+            var numero = int.Parse(Console.ReadLine());
+            if (numero>=0)
+            {
+                Console.WriteLine($"{numero} es positivo");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} es negativo");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
